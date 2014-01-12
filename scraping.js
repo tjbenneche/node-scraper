28c0e70c1b7d29109246c2a53a2175792d536b21
Tom Benneche <tom@tjbenneche.com>
2014-01-12 11:16:53 -0600

    saves each bill in separate, numbered pdf

--- scraping.js
+++ scraping.js
@@ -1,12 +1,21 @@
 var request = require('request'),
 	cheerio = require('cheerio'),
+	http = require('http'),
+	fs = require('fs'),
 	urls = [];
+	var i = 0;
+	
 	
 request('http://docs.house.gov/floor/', function(err, resp, body){
 	if(!err && resp.statusCode == 200){
 		var $ = cheerio.load(body);
 		$('a', '.floorItem').each(function(){
 			var url = this.attr('href');
+			var file = fs.createWriteStream(i + ".pdf")
+			var request = http.get(url, function(response) {
+			  response.pipe(file);
+			});
+			i++;
 			urls.push(url);
 		});
 		console.log(urls);
