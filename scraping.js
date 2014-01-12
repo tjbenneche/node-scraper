ecf2a661ad9a43d87d37967278bda895666dc573
Tom Benneche <tom@tjbenneche.com>
2014-01-12 11:22:58 -0600

    added if statement to only grabpdfs

--- scraping.js
+++ scraping.js
@@ -6,16 +6,20 @@ var request = require('request'),
 	var i = 0;
 	
 	
+	
 request('http://docs.house.gov/floor/', function(err, resp, body){
 	if(!err && resp.statusCode == 200){
 		var $ = cheerio.load(body);
 		$('a', '.floorItem').each(function(){
 			var url = this.attr('href');
+			var fileExt = url.substr(url.lastIndexOf('.')+1);
+			if(fileExt == "pdf"){
 			var file = fs.createWriteStream(i + ".pdf")
 			var request = http.get(url, function(response) {
 			  response.pipe(file);
 			});
 			i++;
+		}
 			urls.push(url);
 		});
 		console.log(urls);
