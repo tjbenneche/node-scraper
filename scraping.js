73b6f60d1ece888e00960664f00eadb4fc9915cc
Tom Benneche <tom@tjbenneche.com>
2014-01-13 17:27:51 -0600

    grabs date, makes new directory for that week

--- scraping.js
+++ scraping.js
@@ -2,20 +2,15 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	http = require('http'),
 	fs = require('fs'),
-	urls = [];
-	var i = 0;
+	urls = [],
+	i = 0;
 	
 //check for date by selecting h1 with attr xmlns:dt="http://xsltsl.org/date-time"
 //if there does not exist a directory in root with name of that date, create one
-/*
-var d = new Date();
-var df = d.getFullYear()
-    + ('0' + String(d.getMonth()+1)).substr(-2)
-    + ('0' + String(d.getDate())).substr(-2);
-alert(df);
 
-fs.mkdir(week);
-*/
+
+
+
 //else return
 
 	
@@ -23,6 +18,20 @@ fs.mkdir(week);
 request('http://docs.house.gov/floor/', function(err, resp, body){
 	if(!err && resp.statusCode == 200){
 		var $ = cheerio.load(body);
+		
+		var title = $("#primaryContent > h1").text();
+		//var name = $(title).toString();
+		var spans = $('.lastUpdated').toString();
+		var extraText = 'Text of Bills for the Week of';
+		var h1close = '</h1>';
+		var week = title.replace(spans,'');
+		week.replace(extraText, '');
+		week.replace(h1close, '');
+		week = './' + week;
+		console.log(week);
+
+		fs.mkdir(week, "0777");
+		
 		$('a', '.floorItem').each(function(){
 			var url = this.attr('href');
 			var fileExt = url.substr(url.lastIndexOf('.')+1);
@@ -36,7 +45,6 @@ request('http://docs.house.gov/floor/', function(err, resp, body){
 			var request = http.get(url, function(response) {
 			  response.pipe(file);
 			});
-			i++;
 		}
 			urls.push(url);
 		});
