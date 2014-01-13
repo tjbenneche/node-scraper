ffaf8b452678099ad2ba80447250a23606fd1b94
Tom Benneche <tom@tjbenneche.com>
2014-01-13 15:19:51 -0600

    trying to grab bill number and date

--- scraping.js
+++ scraping.js
@@ -5,6 +5,19 @@ var request = require('request'),
 	urls = [];
 	var i = 0;
 	
+//check for date by selecting h1 with attr xmlns:dt="http://xsltsl.org/date-time"
+//if there does not exist a directory in root with name of that date, create one
+/*
+var d = new Date();
+var df = d.getFullYear()
+    + ('0' + String(d.getMonth()+1)).substr(-2)
+    + ('0' + String(d.getDate())).substr(-2);
+alert(df);
+
+fs.mkdir(week);
+*/
+//else return
+
 	
 	
 request('http://docs.house.gov/floor/', function(err, resp, body){
@@ -14,6 +27,10 @@ request('http://docs.house.gov/floor/', function(err, resp, body){
 			var url = this.attr('href');
 			var fileExt = url.substr(url.lastIndexOf('.')+1);
 			if(fileExt == "pdf"){
+			//trying to make more meaningful name with bill #
+			var name = $(this).closest('.legisNum');
+			var bill = $(name).text();
+			console.log(bill);
 			var file = fs.createWriteStream(i + ".pdf")
 			var request = http.get(url, function(response) {
 			  response.pipe(file);
@@ -25,3 +42,4 @@ request('http://docs.house.gov/floor/', function(err, resp, body){
 		console.log(urls);
 	}
 });
+
